1854268dd3b4c9e59d6a4fca9c9877c6cb612704
awalkowskeeD <alex@walkowski.io>
2025-09-06 20:18:09 -0400

    trying to understand this whole thing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+// Grab the root div from public/index.html
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Render your main App component
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
